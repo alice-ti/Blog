be8da3638ad36682483b61fcc35c19f58fd230c9
alice-ti <alice-ti@users.noreply.github.com>
2025-05-30 08:45:01 +0000

    deploy: 3a7b07f34b7c3ccca1c4f5bd62213ee670783d97

---
+++ assets/docs_Python_04-pipx.md.D8idY10H.js
@@ -0,0 +1,14 @@
+import{_ as a,c as i,o as n,af as p}from"./chunks/framework.BivmJ8bT.js";const r=JSON.parse('{"title":"PIPX","description":"","frontmatter":{},"headers":[],"relativePath":"docs/Python/04-pipx.md","filePath":"docs/Python/04-pipx.md","lastUpdated":1748594539000}'),l={name:"docs/Python/04-pipx.md"};function e(t,s,h,E,c,k){return n(),i("div",null,s[0]||(s[0]=[p(`<h1 id="pipx" tabindex="-1">PIPX <a class="header-anchor" href="#pipx" aria-label="Permalink to &quot;PIPX&quot;">​</a></h1><p>pipx 的主要用途：</p><ul><li>用于安装和运行 Python 应用程序</li><li>每个应用都在独立的虚拟环境中运行，避免依赖冲突</li></ul><div class="language-cmd vp-adaptive-theme"><button title="Copy Code" class="copy"></button><span class="lang">cmd</span><pre class="shiki shiki-themes github-light github-dark vp-code" tabindex="0"><code><span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"># 安装包</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">pipx install package_name</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"># 列出已安装的包</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">pipx list</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"># 运行一个包但不安装</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">pipx run package_name</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"># 卸载包</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">pipx uninstall package_name</span></span>
+<span class="line"></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;"># 升级所有包</span></span>
+<span class="line"><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">pipx upgrade</span><span style="--shiki-light:#D73A49;--shiki-dark:#F97583;">-</span><span style="--shiki-light:#24292E;--shiki-dark:#E1E4E8;">all</span></span></code></pre></div>`,4)]))}const o=a(l,[["render",e]]);export{r as __pageData,o as default};
